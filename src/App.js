cadb3ab585b5289eab26d7f20ad20eb61c9cc396
wmaiduo <wmaiduo@gmail.com>
2020-05-27 13:03:46 -0700

    journal-app
    Journal app created using react and redux

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+function App() {
+  return (
+    <Jumbotron>
+      Hello
+    </Jumbotron>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+export default class Navbar extends React.Component{
+    
+}
