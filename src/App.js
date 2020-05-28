97e6132a42f0666b73059fe652e52c7c3e623a9c
wmaiduo <wmaiduo@gmail.com>
2020-05-27 17:40:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+
+import EntryPage from "./pages/index"
+import ErrorPage from "./pages/404"
+import FrontPage from "./pages/front"
 
 function App() {
   return (
-    <Jumbotron>
-      Hello
-    </Jumbotron>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={EntryPage} />
+        <Route exact path="/front" component={FrontPage} />
+        <Route exact path="/404" component={ErrorPage} />
+        <Redirect to ="/404" />
+      </Switch>
+    </Router>
   );
 }
 
