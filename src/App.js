eb3ee941a4bc569da07794c7cfe436037ac458d8
wmaiduo <wmaiduo@gmail.com>
2020-05-29 22:35:56 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import EntryPage from "./pages/index"
 import ErrorPage from "./pages/404"
 import FrontPage from "./pages/front"
-
+import SubmitPage from "./pages/newjournal"
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={EntryPage} />
         <Route exact path="/front" component={FrontPage} />
+        <Route exact path ="/submit" component={SubmitPage} />
         <Route exact path="/404" component={ErrorPage} />
         <Redirect to ="/404" />
       </Switch>
