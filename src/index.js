e9ab814331857ffa5968addc7ad21620a06a8785
wmaiduo <wmaiduo@gmail.com>
2020-05-29 22:36:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
+
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './redux/reducer'
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+export default store
